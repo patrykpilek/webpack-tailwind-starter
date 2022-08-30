463580722adb0b224604f4954205a1850451d0c3
Patryk Pilek <patryk.pilek@gmail.com>
2022-08-30 22:35:28 +0100

    Update webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
+    // mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
